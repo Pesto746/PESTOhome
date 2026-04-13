00bd386dcb65cb1c03200c5c7b0c0994b5d65ee2
Pesto746 <Pesto.ghadeer@outlook.com>
2026-04-13 17:19:04 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,7 @@
-const CACHE_NAME = "pwa-cache-v1";
-const urlsToCache = [
-  "/",
-  "/styles.css",
-  "/script.js",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Installed');
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+  // Required for the "Install" button to appear
 });
